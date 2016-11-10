4344f68fe77c27813f4e1268e5e14a7ed4a9ba89
blue-alien-bg <blue_alien@abv.bg>
2016-11-10 22:14:21 +0100

    Last updates before sleep

--- assignments/Assignment01/app.js
+++ assignments/Assignment01/app.js
@@ -6,7 +6,18 @@
 
   MsgController.$inject = ['$scope'];
   function MsgController($scope){
-    
+    $scope.myText = "";
+    $scope.message = "";
+    $scope.tooMuch = function() {
+      if($scope.myText == "")
+        $scope.message = "Please enter data first";
+      else{
+        $scope.message = "There is data";
+      }
+    }
+    // $scope.logger = function() {
+    //   console.log($scope.myText);
+    // }
   }
 
 })();
