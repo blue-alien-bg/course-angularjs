efdac27f4e5c7ba47c52d1635acbf6a024d882f3
blue-alien-bg <blue_alien@abv.bg>
2016-11-13 20:32:08 +0100

    First assingnment finished

--- assignments/Assignment01/app.js
+++ assignments/Assignment01/app.js
@@ -6,18 +6,41 @@
 
   MsgController.$inject = ['$scope'];
   function MsgController($scope){
+    $scope.colour = "black";
     $scope.myText = "";
     $scope.message = "";
     $scope.tooMuch = function() {
-      if($scope.myText == "")
+      if($scope.myText == ""){
         $scope.message = "Please enter data first";
+        $scope.colour = "red";
+      }
       else{
-        $scope.message = "There is data";
+        if(itemsCount($scope.myText.split(",")) > 0){
+          if(itemsCount($scope.myText.split(",")) > 3){
+            $scope.message = "Too much!";
+            $scope.colour = "green";
+          }
+          else{
+            $scope.message = "Enjoy!";
+            $scope.colour = "green";
+          }
+        }
+        else{
+          $scope.message = "Aren't you hungry?";
+          $scope.colour = "red";
+        }
       }
     }
-    // $scope.logger = function() {
-    //   console.log($scope.myText);
-    // }
+  }
+
+  function itemsCount(items){
+    var result = 0, i = 0;
+    while (items[i] != undefined) {
+      if(items[i] != "" && items[i] != " ")
+        result++;
+      i++;
+    }
+    return result;
   }
 
 })();
