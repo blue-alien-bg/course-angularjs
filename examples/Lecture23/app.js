271e631973e453739d2ef16fa4dcfac05ca65d60
blue-alien-bg <blue_alien@abv.bg>
2016-11-26 02:02:20 +0100

    Lecture 23

---
+++ examples/Lecture23/app.js
@@ -0,0 +1,76 @@
+(function(){
+'use strict';
+
+  angular.module("ControllerAsApp", [])
+    .controller("ShoppingListController", ShoppingListController)
+    .provider("ShoppingListService", ShoppingListServiceProvider)
+    .config(Config);
+
+  Config.$inject = ["ShoppingListServiceProvider"];
+  function Config(ShoppingListServiceProvider){
+    ShoppingListServiceProvider.defaults.maxItems = 1;
+  }
+
+  ShoppingListController.$inject = ["ShoppingListService"];
+  function ShoppingListController(ShoppingListService){
+    var list = this;
+
+    list.items = ShoppingListService.getItems();
+
+    list.itemName = "";
+    list.itemQuantity = "";
+
+    list.addItem = function(){
+      try {
+        ShoppingListService.addItem(list.itemName, list.itemQuantity);
+        list.itemName = "";
+        list.itemQuantity = "";
+      } catch (error) {
+          list.errorMessage = error.message;
+      }
+    };
+
+    list.removeItem = function(itemIndex){
+      ShoppingListService.removeItem(itemIndex);
+    };
+
+  }
+
+  // If not specified maxItems assumed unlimited
+  function ShoppingListService(maxItems){
+    var service = this;
+
+    var items = [];
+
+    service.addItem = function(itemName, itemQuantity){
+      if(maxItems === undefined || maxItems !== undefined && items.length < maxItems){
+        var item = {name: itemName, quantity: itemQuantity};
+        items.push(item);
+      }
+      else{
+        throw new Error("Max items (" + maxItems + ") reached.");
+      }
+    };
+
+    service.removeItem = function(itemIndex){
+      items.splice(itemIndex, 1);
+    };
+
+    service.getItems = function(){
+      return items;
+    };
+  }
+
+  function ShoppingListServiceProvider(){
+    var provider = this;
+
+    provider.defaults = {maxItems: 10};
+
+    provider.$get = function(){
+      var shoppingList = new ShoppingListService(provider.defaults.maxItems);
+
+      return shoppingList;
+    };
+  }
+
+})();
